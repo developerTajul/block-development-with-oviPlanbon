a1cd55622d60d50da30b60acfdd1a42601864aa5
developerTajul <developer.tajul@gmail.com>
2019-03-24 11:40:50 +0600

    Alerbox with PlainText

---
+++ src/alert/helper.js
@@ -0,0 +1,46 @@
+export const STYLES = [
+		{
+	        name: 'primary',
+	        label: __( 'Primary' ),
+	        isDefault: true
+	    },
+	    {
+	        name: 'secondary',
+	        label: __( 'Secondary' )
+	    },
+	    {
+	        name: 'success',
+	        label: __( 'Success' )
+	    },
+	    {
+	        name: 'danger',
+	        label: __( 'Danger' )
+	    },
+	    {
+	        name: 'warning',
+	        label: __( 'Warning' )
+	    },
+	    {
+	        name: 'info',
+	        label: __( 'Info' )
+	    },
+	    {
+	        name: 'light',
+	        label: __( 'Light' )
+	    },
+	    {
+	        name: 'light',
+	        label: __( 'Light' )
+	    },
+	    {
+	        name: 'dark',
+	        label: __( 'Dark' )
+	    }
+	];
+
+
+export function getStyleNameFromClasses(styles, classes){
+	let style = (classes && styles.find(style => classes.includes(style.name)));
+
+	return (style ? style.name : 'primary');
+}
